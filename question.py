a089553a66c707602b7019b1de21d8a59bbc8d62
hashashin <hubermanandrew5@gmail.com>
2025-06-09 12:21:11 +0800

    Auto-sync: 2025-06-09 12:21:11

--- question.py
+++ question.py
@@ -1,15 +1,21 @@
-# Please install OpenAI SDK first: `pip3 install openai`
-
+import os
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-d4c80c767d914bdc8dad316f9ec6100c", base_url="https://api.deepseek.com")
-
+client = OpenAI(
+    # 从环境变量中读取您的方舟API Key
+    api_key=os.environ.get("37946450-91ae-4a14-a7e6-3728cc785904"), 
+    base_url="https://ark.cn-beijing.volces.com/api/v3",
+    # 深度思考模型耗费时间会较长，建议您设置一个较长的超时时间，推荐为30分钟
+    timeout=1800,
+    )
 response = client.chat.completions.create(
-    model="deepseek-chat",
+    # 替换 <Model> 为 Model ID
+    model="Doubao-1.5-lite-32k",
     messages=[
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False
+        {"role": "user", "content": "我要研究深度思考模型与非深度思考模型区别的课题，怎么体现我的专业性"}
+    ]
 )
-
+# 当触发深度思考时，打印思维链内容
+if hasattr(response.choices[0].message, 'reasoning_content'):
+    print(response.choices[0].message.reasoning_content)
 print(response.choices[0].message.content)
